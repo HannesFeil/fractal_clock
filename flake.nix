0a3635a99ba28eea39fa42d09a474d39aff007bf
HannesFeil <hannesfeil@gmx.de>
2024-04-08 00:32:17 +0200

    Removing nixGL and adding vulkan backend (which works?)

--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixgl }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: 
       let 
         pkgs = import nixpkgs { inherit system; };
@@ -17,12 +16,10 @@
             buildInputs = [
               libxkbcommon
               libGL
+              vulkan-loader
 
               # WINIT_UNIX_BACKEND=wayland
               wayland          
-            ];
-            packages = [
-              (pkgs.lib.traceVal nixgl.packages.${system}).default
             ];
             LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
           };
